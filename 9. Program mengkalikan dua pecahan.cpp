91c8eea5c86ef70a54e2a40326d5aa8ac64194af
Muhammad Dicky Garcia <45659379+dicgar95@users.noreply.github.com>
2018-12-27 23:57:00 +0700

    Add files via upload

---
+++ 9. Program mengkalikan dua pecahan.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  float hasil;
+  std::cout << "p1=3.0/4.0" << '\n';
+  std::cout << "p2=2.0/3.0" << '\n';
+hasil=(3.0*2.0)/(3.0*4.0);
+std::cout << "hasilnya adalah :"<<hasil << '\n';
+  return 0;
+}
